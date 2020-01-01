06bf612838c0054187f1f6ff689131a098739960
docaohuynh <docaohuynh.cse@gmail.com>
2020-01-01 12:05:13 +0700

    tslint

--- src/environments/environment.serverless.ts
+++ src/environments/environment.serverless.ts
@@ -15,5 +15,5 @@ export const environment = {
     appId: '1:123494147327:web:1390951fda5393dd2e98ca',
     measurementId: 'G-G86MLL9HYE'
   },
-	baseHref: '/production/'
+  baseHref: '/production/'
 };
